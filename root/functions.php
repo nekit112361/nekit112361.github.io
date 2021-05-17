47944605a0ad1eda9d53c731470602ec3042eb88
nekit112 <56692626+nekit112361@users.noreply.github.com>
2021-05-17 03:49:44 +0300

    Delete functions.php

--- root/functions.php
+++
@@ -1,7 +0,0 @@
-/**
-Enqueues scripts video-background.
-*/
-function webkato_scripts() {
-wp_enqueue_script( 'video-background', get_template_directory_uri() . '/js/video-background.js', array('jquery'), true );
-}
-add_action( 'wp_enqueue_scripts', 'webkato_scripts' );
